1004b95b2651fa92ff1dee6eee48bda33f701af6
Craig Swank <craigswank@gmail.com>
2016-05-14 08:02:29 -0600

    added search filter

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 	"golang.org/x/crypto/ssh/agent"
 	"golang.org/x/crypto/ssh/terminal"
 
-	"github.com/gizak/termui"
+	ui "github.com/gizak/termui"
 
 	"gopkg.in/alecthomas/kingpin.v2"
 )
@@ -67,41 +67,138 @@ func getRows(body io.Reader) []string {
 }
 
 func showResults(rows []string) string {
-	width := getWidth(rows)
-	err := termui.Init()
+	//origRows := rows
+
+	err := ui.Init()
 	if err != nil {
 		panic(err)
 	}
-	defer termui.Close()
+	defer ui.Close()
 
-	ls := termui.NewList()
+	searchText := []string{""}
+	ls, f := getLists(rows, searchText)
+
+	ui.Render(ls, f)
+
+	var result string
+	var searchMode, confirmMode bool
+	var indexStr string
+
+	ui.Handle("/sys/kbd/C-d", func(ui.Event) {
+		ui.StopLoop()
+	})
+
+	ui.Handle("/sys/kbd/C-f", func(ui.Event) {
+		searchMode = true
+		f.BorderLabel = "Filter (enter when done)"
+		ui.Render(ls, f)
+	})
+
+	ui.Handle("/sys/kbd", func(e ui.Event) {
+		if searchMode {
+			s := getString(e.Data)
+			if s == "C-8" {
+				end := len(searchText[0]) - 1
+				if end < 0 {
+					end = 0
+				}
+				searchText[0] = searchText[0][0:end]
+			} else if s == "<enter>" {
+				searchMode = false
+				f.BorderLabel = "Filter (C-f)"
+			} else {
+				searchText[0] += s
+			}
+			if len(searchText[0]) > 0 {
+				ls.Items = filter(rows, searchText[0])
+			} else {
+				ls.Items = rows
+			}
+			ui.Render(ls, f)
+		} else if confirmMode {
+			s := getString(e.Data)
+			if s == "y" {
+				ui.StopLoop()
+				return
+			} else {
+				result = ""
+				ui.Clear()
+				ui.Render(ls, f)
+			}
+		} else {
+			s := getString(e.Data)
+			if s == "<enter>" {
+				j, err := strconv.ParseInt(indexStr, 10, 64)
+				if err != nil {
+					//alert
+					return
+				}
+				i := int(j)
+				if i <= len(rows) && i > 0 {
+					result = rows[i-1]
+					confirmMode = true
+					c := getConfrm(result)
+					indexStr = ""
+					ui.Clear()
+					ui.Render(c)
+				}
+			} else {
+				indexStr += getString(e.Data)
+			}
+		}
+	})
+
+	ui.Loop()
+	return result
+}
+
+func getConfrm(result string) *ui.List {
+	parts := strings.Split(result, " ")
+	label := fmt.Sprintf("Really ssh to %s (y/n)?", parts[1])
+	c := ui.NewList()
+	c.Items = []string{}
+	c.ItemFgColor = ui.ColorYellow
+	c.BorderLabel = label
+	c.Height = 3
+	c.Width = len(label) + 4
+	c.Y = 0
+	return c
+}
+
+func getLists(rows []string, searchText []string) (*ui.List, *ui.List) {
+	width := getWidth(rows)
+
+	ls := ui.NewList()
 	ls.Items = rows
-	ls.ItemFgColor = termui.ColorYellow
-	ls.BorderLabel = "Results"
+	ls.ItemFgColor = ui.ColorYellow
+	ls.BorderLabel = "Results (C-d to exit)"
 	ls.Height = len(rows) + 2
 	ls.Width = width + 4
 	ls.Y = 0
 
-	var result string
+	f := ui.NewList()
+	f.Items = searchText
+	f.ItemFgColor = ui.ColorRed
+	f.BorderLabel = "Filter (C-f)"
+	f.Height = 3
+	f.Width = width + 4
+	f.Y = len(rows) + 2
+	return ls, f
+}
 
-	termui.Render(ls)
-
-	termui.Handle("/sys/kbd/C-d", func(termui.Event) {
-		termui.StopLoop()
-	})
-
-	termui.Handle("/sys/kbd", func(e termui.Event) {
-		i, err := getIndex(e.Data)
-		if err != nil {
-			//alert
-			return
+func filter(rows []string, sep string) []string {
+	var out []string
+	for _, r := range rows {
+		if strings.Index(r, sep) > -1 {
+			out = append(out, r)
 		}
-		termui.StopLoop()
-		result = rows[i-1]
-	})
+	}
+	return out
+}
 
-	termui.Loop()
-	return result
+func getString(d interface{}) string {
+	k := strings.Replace(fmt.Sprintf("%s", d), "{", "", -1)
+	return strings.Replace(k, "}", "", -1)
 }
 
 func getWidth(rows []string) int {
@@ -114,10 +211,11 @@ func getWidth(rows []string) int {
 	return max
 }
 
-func getIndex(d interface{}) (int64, error) {
+func getIndex(d interface{}) (int, error) {
 	k := strings.Replace(fmt.Sprintf("%s", d), "{", "", -1)
 	k = strings.Replace(k, "}", "", -1)
-	return strconv.ParseInt(k, 10, 64)
+	i, err := strconv.ParseInt(k, 10, 64)
+	return int(i), err
 }
 
 func login(host string) {
