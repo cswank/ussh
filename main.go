36127f260f77fadd43e656f861b2b6954e04fac9
Craig Swank <craigswank@gmail.com>
2016-05-15 14:38:05 -0600

    wip

--- main.go
+++ main.go
@@ -38,6 +38,13 @@ var (
 			ssh:    ui.ColorGreen,
 		},
 	}
+	mode int
+)
+
+const (
+	modeDefault int = iota
+	modeSearch
+	modeConfirm
 )
 
 type theme struct {
@@ -55,6 +62,25 @@ type resp struct {
 	Results []result `json:"results"`
 }
 
+type resultList struct {
+	rows   []string
+	filter string
+	list   *ui.List
+}
+
+type filterList struct {
+	sep  string
+	rows []string
+	list *ui.List
+}
+
+type sshList struct {
+	rows   []string
+	index  string
+	list   *ui.List
+	target string
+}
+
 func init() {
 	var ok bool
 	userTheme, ok = themes[os.Getenv("UPTIME_THEME")]
@@ -65,7 +91,6 @@ func init() {
 
 func main() {
 	kingpin.Parse()
-
 	q := strings.Replace(*query, "%", "%25", -1)
 	r, err := http.Get(fmt.Sprintf("%s/servers/search/%s/%s", addr, q, key))
 	if err != nil {
@@ -80,13 +105,6 @@ func main() {
 	}
 }
 
-func getOffset(i int) int {
-	if i < 10 {
-		return 5
-	}
-	return 6
-}
-
 func getRows(body io.Reader) []string {
 	var r resp
 	dec := json.NewDecoder(body)
@@ -94,118 +112,87 @@ func getRows(body io.Reader) []string {
 		log.Fatal(err)
 	}
 	rows := make([]string, len(r.Results))
+
 	for i, x := range r.Results {
-		t := fmt.Sprintf("[%%d] %-%%ds", getOffset(i))
-		rows[i] = fmt.Sprintf(t, i+1, x.Host)
+		rows[i] = x.Host
 	}
+	fmt.Println(rows)
 	return rows
 }
 
 func showResults(rows []string) string {
-	//origRows := rows
-
 	err := ui.Init()
 	if err != nil {
 		panic(err)
 	}
 	defer ui.Close()
 
-	searchText := []string{""}
-	ls, f, t := getLists(rows, searchText)
+	ls, f, t := getLists(rows)
 
-	ui.Render(ls, f, t)
-
-	var result string
-	var searchMode, confirmMode bool
-	var indexStr string
+	ui.Render(ls.list, f.list, t.list)
 
 	ui.Handle("/sys/kbd/C-d", func(ui.Event) {
-		result = ""
+		t.target = ""
 		ui.StopLoop()
 	})
 
 	ui.Handle("/sys/kbd/C-f", func(ui.Event) {
-		searchMode = true
-		f.BorderLabel = "Filter (enter when done)"
-		ui.Render(ls, f)
+		mode = modeSearch
+		f.list.BorderLabel = "Filter (enter when done)"
+		ui.Render(ls.list, f.list, t.list)
 	})
 
 	ui.Handle("/sys/kbd", func(e ui.Event) {
-		if searchMode {
-			s := getString(e.Data)
-			if s == "C-8" {
-				end := len(searchText[0]) - 1
-				if end < 0 {
-					end = 0
-				}
-				searchText[0] = searchText[0][0:end]
-			} else if s == "<enter>" {
-				searchMode = false
-				f.BorderLabel = "Filter (C-f)"
-			} else {
-				searchText[0] += s
-			}
-			if len(searchText[0]) > 0 {
-				ls.Items = filter(rows, searchText[0])
-			} else {
-				ls.Items = rows
-			}
-			ui.Render(ls, f)
-		} else if confirmMode {
-			s := getString(e.Data)
+		s := getString(e.Data)
+		switch mode {
+		case modeSearch:
+			f.filter(s, ls)
+		case modeConfirm:
 			if s == "y" {
 				ui.StopLoop()
 				return
-			} else {
-				result = ""
-				ui.Clear()
-				ui.Render(ls, f, t)
 			}
-		} else {
-			defer func() {
-				if !confirmMode && indexStr != "" {
-					j, err := strconv.ParseInt(indexStr, 10, 64)
-					if err != nil {
-						j = -1
-					}
-					i := int(j)
-					if i <= len(rows) && i > 0 {
-						t.BorderLabel = fmt.Sprintf("ssh to %s (enter number)", indexStr)
-						result = rows[i-1]
-						parts := strings.Split(result, " ")
-						t.Items = []string{parts[1]}
-
-					}
-				} else {
-					t.BorderLabel = "ssh to (enter number)"
-					t.Items = []string{""}
-				}
-				if !confirmMode {
-					ui.Render(ls, f, t)
-				}
-			}()
-			s := getString(e.Data)
-			if s == "<enter>" {
-				confirmMode = true
-				c := getConfrm(result)
-				indexStr = ""
-				ui.Clear()
-				ui.Render(c)
-			} else if s == "C-8" {
-				indexStr = ""
-			} else {
-				indexStr += getString(e.Data)
-			}
+			mode = modeDefault
+			t.target = ""
+			ui.Clear()
+			ui.Render(ls.list, f.list, t.list)
+		default:
+			t.getResult(s)
+			ui.Render(ls.list, f.list, t.list)
 		}
 	})
-
 	ui.Loop()
-	return result
+	return t.target
 }
 
-func getConfrm(result string) *ui.List {
-	parts := strings.Split(result, " ")
-	label := fmt.Sprintf("Really ssh to %s (y/n)?", parts[1])
+func (s *sshList) getResult(in string) {
+	switch in {
+	case "<enter>":
+		s.showConfirm()
+	case "C-8":
+		s.index = ""
+		s.target = ""
+		s.list.BorderLabel = "ssh to (enter number)"
+		s.list.Items = []string{""}
+	default:
+		s.index += getString(in)
+		j, err := strconv.ParseInt(s.index, 10, 64)
+		if err != nil {
+			return
+		}
+		i := int(j)
+		if !(i <= len(s.rows) || i > 0) {
+			return
+		}
+		s.target = s.rows[i]
+		s.list.Items = []string{s.target}
+		s.list.BorderLabel = fmt.Sprintf("ssh to %s (enter number)", s.index)
+	}
+}
+
+func (s *sshList) showConfirm() {
+	mode = modeConfirm
+	label := fmt.Sprintf("Really ssh to %s (y/n)?", s.target)
 	c := ui.NewList()
 	c.Items = []string{}
 	c.ItemFgColor = ui.ColorYellow
@@ -213,47 +200,39 @@ func getConfrm(result string) *ui.List {
 	c.Height = 3
 	c.Width = len(label) + 4
 	c.Y = 0
-	return c
+	ui.Clear()
+	ui.Render(c)
 }
 
-func getLists(rows []string, searchText []string) (*ui.List, *ui.List, *ui.List) {
-	width := getWidth(rows)
-
-	ls := ui.NewList()
-	ls.Items = rows
-	ls.ItemFgColor = userTheme.list
-	ls.BorderLabel = "sesults (C-d to exit)"
-	ls.Height = len(rows) + 2
-	ls.Width = width + 4
-	ls.Y = 0
-
-	f := ui.NewList()
-	f.Items = searchText
-	f.ItemFgColor = userTheme.filter
-	f.BorderLabel = "filter (C-f)"
-	f.Height = 3
-	f.Width = width + 4
-	f.Y = len(rows) + 2
-
-	s := ui.NewList()
-	s.Items = searchText
-	s.ItemFgColor = userTheme.ssh
-	s.BorderLabel = "ssh to (enter number)"
-	s.Height = 3
-	s.Width = width + 4
-	s.Y = len(rows) + 5
-
-	return ls, f, s
+func (f *filterList) delete() {
+	end := len(f.sep) - 1
+	if end < 0 {
+		end = 0
+	}
+	f.sep = f.sep[0:end]
 }
 
-func filter(rows []string, sep string) []string {
+func (f *filterList) enter() {
+	mode = modeDefault
+	f.list.BorderLabel = "Filter (C-f)"
+}
+
+func (f *filterList) filter(s string, l *resultList) {
+	switch s {
+	case "C-8":
+		f.delete()
+	case "<ender>":
+		f.enter()
+	default:
+		f.sep += s
+	}
 	var out []string
-	for _, r := range rows {
-		if strings.Index(r, sep) > -1 {
+	for _, r := range f.rows {
+		if strings.Index(r, f.sep) > -1 {
 			out = append(out, r)
 		}
 	}
-	return out
+	l.list.Items = out
 }
 
 func getString(d interface{}) string {
@@ -342,4 +321,47 @@ func login(host string) {
 	}
 
 	session.Wait()
+}
+
+func getLists(rows []string) (*resultList, *filterList, *sshList) {
+	width := getWidth(rows)
+
+	ls := ui.NewList()
+	ls.Items = rows
+	ls.ItemFgColor = userTheme.list
+	ls.BorderLabel = "sesults (C-d to exit)"
+	ls.Height = len(rows) + 2
+	ls.Width = width + 4
+	ls.Y = 0
+
+	f := ui.NewList()
+	f.Items = []string{""}
+	f.ItemFgColor = userTheme.filter
+	f.BorderLabel = "filter (C-f)"
+	f.Height = 3
+	f.Width = width + 4
+	f.Y = len(rows) + 2
+
+	s := ui.NewList()
+	s.Items = []string{""}
+	s.ItemFgColor = userTheme.ssh
+	s.BorderLabel = "ssh to (enter number)"
+	s.Height = 3
+	s.Width = width + 4
+	s.Y = len(rows) + 5
+
+	return &resultList{rows: rows, list: ls}, &filterList{rows: rows, list: f}, &sshList{rows: rows, list: s}
+}
+
+func getConfrm(result string) *ui.List {
+	parts := strings.Split(result, " ")
+	label := fmt.Sprintf("Really ssh to %s (y/n)?", parts[1])
+	c := ui.NewList()
+	c.Items = []string{}
+	c.ItemFgColor = ui.ColorYellow
+	c.BorderLabel = label
+	c.Height = 3
+	c.Width = len(label) + 4
+	c.Y = 0
+	return c
 }
